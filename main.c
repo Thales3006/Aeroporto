ff41ce9d4530b29d26f35ad69a04818abb32481e
Thales Janisch Santos <33602977+Thales3006@users.noreply.github.com>
2023-11-29 20:08:21 -0300

    main.c mais que finalizada

--- main.c
+++ main.c
@@ -2,52 +2,8 @@
 #include<stdlib.h>
 #include<string.h>
 #include"bibs.h"
-#define ARRAY_SIZE 100
 
-void parse(pista** aeroporto, aviao** ceu, aviao** pousados,FILE* entrada){
-    char* cmd_line = malloc(sizeof(char)*ARRAY_SIZE);
-    char* comando = malloc(sizeof(char)*ARRAY_SIZE);
-    int* argI = malloc(sizeof(int)*4);
-    char* argS = malloc(sizeof(char)*2*ARRAY_SIZE);
-
-    if(!cmd_line||!comando||!argI||!argS){
-        printf("\nSem memoria o suficiente!\n");
-        exit(1);
-    }
-
-    while( feof(entrada) == 0 ){
-        fgets(cmd_line, 100, entrada);
-
-        strcpy(comando,strtok(cmd_line," "));
-
-        if(strcmp(comando,"pistas")==0){
-            *(argI+0)= atoi(strtok(NULL," "));
-            pistas(aeroporto, argI[0] );
-        }
-        else if(strcmp(comando,"insere_aviao")==0){
-            *(argI+0)= atoi(strtok(NULL," "));
-            *(argI+1)= atoi(strtok(NULL," "));
-            strcpy((argS+0*ARRAY_SIZE), strtok(NULL," "));
-            strcpy((argS+1*ARRAY_SIZE), strtok(NULL," "));
-            *(argI+2)= atoi(strtok(NULL," "));
-            *(argI+3)= atoi(strtok(NULL," "));
-
-            add_to_pista(aeroporto, *(argI+0)-1, *(argI+1), (argS+0*ARRAY_SIZE), (argS+1*ARRAY_SIZE), *(argI+2), *(argI+3), 0, 1, 0);
-        }
-        else if(strcmp(comando,"decolar")==0){
-            *(argI+0)= atoi(strtok(NULL," "));
-
-            decolar(aeroporto, ceu, *(argI+0)-1);
-        }
-        else if(strcmp(comando,"iniciasimulacao")==0){
-            *(argI+0)= atoi(strtok(NULL," "));
-            iniciasimulacao(aeroporto, ceu, pousados, *(argI+0));
-        }
-        else if(strcmp(comando,"fim")==0){
-            fim(aeroporto,ceu,pousados);
-        }
-    }
-}
+void parse(pista** aeroporto, aviao** ceu, aviao** pousados,FILE* entrada);
 
 int main(){
     pista* aeroporto = NULL;
@@ -61,11 +17,75 @@ int main(){
 
     parse(&aeroporto, &voando, &pousados, entrada);
 
-    printf("\n");
-    imprime_pista(aeroporto);
-    printf("VOANDO:\n");
-    imprime_aviao(voando);
-
     return 0;
 }
 
+void parse(pista** aeroporto, aviao** ceu, aviao** pousados,FILE* entrada){
+    char* cmd_line = malloc(sizeof(char)*ARRAY_SIZE);
+    char* comando = malloc(sizeof(char)*ARRAY_SIZE);
+    int* argI = malloc(sizeof(int)*4);
+    char* argS = malloc(sizeof(char)*2*ARRAY_SIZE);
+    int* clima = malloc(sizeof(int)*3);
+
+    if(!cmd_line||!comando||!argI||!argS||!clima){
+        printf("\nSem memoria o suficiente!\n");
+        exit(1);
+    }
+
+    *(clima+0)=-1;
+    *(clima+1)=-1;
+    *(clima+2)=-1;
+
+    while( feof(entrada) == 0 ){
+        fgets(cmd_line, 100, entrada);
+
+        strcpy(comando,strtok(cmd_line," "));
+
+        if(strcmp(comando,"pistas")==0){
+            *(argI+0)= atoi(strtok(NULL," "));
+            
+            pistas(aeroporto, argI[0] );
+        }
+
+        else if(strcmp(comando,"insere_aviao")==0){
+            *(argI+0)= atoi(strtok(NULL," "));
+            *(argI+1)= atoi(strtok(NULL," "));
+            strcpy((argS+0*ARRAY_SIZE), strtok(NULL," "));
+            strcpy((argS+1*ARRAY_SIZE), strtok(NULL," "));
+            *(argI+2)= atoi(strtok(NULL," "));
+            *(argI+3)= atoi(strtok(NULL," "));
+
+            add_to_pista(aeroporto, *(argI+0)-1, *(argI+1), (argS+0*ARRAY_SIZE), (argS+1*ARRAY_SIZE), *(argI+2), *(argI+3), 0, 1, 0);
+        }
+
+        else if(strcmp(comando,"decolar")==0){
+            *(argI+0)= atoi(strtok(NULL," "));
+
+            decolar(aeroporto, ceu, *(argI+0)-1);
+        }
+
+        else if(strcmp(comando,"tempestade")==0){
+            *(clima+0)= atoi(strtok(NULL," "));
+            *(clima+0)-= 1;
+        }
+        else if(strcmp(comando,"neblina")==0){
+            *(clima+1)= atoi(strtok(NULL," "));
+            *(clima+1)-= 1;
+        }
+        else if(strcmp(comando,"turbulencia")==0){
+            *(clima+2)= atoi(strtok(NULL," "));
+            *(clima+2)-= 1;
+        }
+
+        else if(strcmp(comando,"iniciasimulacao")==0){
+            *(argI+0)= atoi(strtok(NULL," "));
+
+            iniciasimulacao(aeroporto, ceu, pousados, *(argI+0), clima);
+        }
+        
+        else if(strcmp(comando,"fim")==0){
+            fim(aeroporto,ceu,pousados);
+        }
+    }
+    fim(aeroporto,ceu,pousados);
+}
