4f6106cf260cc12b00aecd8e8563564cb296baa4
Thales Janisch Santos <33602977+Thales3006@users.noreply.github.com>
2023-11-29 20:07:53 -0300

    bib.h finalizada

--- bibs.h
+++ bibs.h
@@ -1,4 +1,7 @@
 
+#define MIN 60
+#define ARRAY_SIZE 100
+
 typedef struct aviao{
     int codigo;
     char modelo[10];
@@ -38,12 +41,24 @@ int tamanho_in_pista(pista* a);
 aviao* info_Pista(pista* a, int posP, int posA);
 aviao* info(aviao* a, int pos);
 
+//********************************************
+
+int menu(pista** aeroporto, aviao **ceu, aviao **pousados, int ciclos);
+
 void pistas(pista** a,int n);
 void decolar(pista** aeroporto, aviao** ceu, int pista);
 void aterrissando(aviao **a, aviao** pousados, int ciclos);
-void iniciasimulacao(pista** aeroporto, aviao** ceu, aviao** pousados, int ciclos);
+void aterrissagem_forcada(aviao **ceu, aviao** pousados, int codigo, int ciclos);
+void iniciasimulacao(pista** aeroporto, aviao** ceu, aviao** pousados, int ciclos, int *clima);
 void aviao_move(aviao* ceu);
+void tempestade(aviao **ceu);
+void neblina(aviao **ceu);
+void turbulencia(aviao **ceu);
+int colisao(aviao* ceu);
+int colidiu(aviao* ceu, float x, float y, float z);
 void fim(pista **aeroporto,aviao **voando,aviao** pousados);
 
 void add_to_log(FILE* log,aviao* a);
-void registrar(aviao **pousados);
+void registrar(pista** aeroporto, aviao **ceu, aviao **pousados, int* contadores);
+
+//*****************************************
